a0673902586b74179e83cf05fdb270dd53388aa2
Mirjana Ristic <mima34.mg@gmail.com>
2026-09-26 13:06:14 +0200

    Add DTOs for Product and Category

---
+++ ShopFullStack.Api/Dtos/CreateCategoryDto.cs
@@ -0,0 +1,6 @@
+﻿namespace ShopFullStack.Api.Dtos;
+
+public class CreateCategoryDto
+{
+    public string Name { get; set; } = string.Empty;
+}
---
+++ ShopFullStack.Api/Dtos/CreateProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopFullStack.Api.Dtos;
+
+public class CreateProductDto
+{
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public int CategoryId { get; set; }
+}
