1b285084b92bf9901a457570a763bf1efbec99bd
Mirjana Ristic <mima34.mg@gmail.com>
2026-09-26 12:49:36 +0200

    Add generic repository pattern with Product and Category repositories, register in DI

---
+++ ShopFullStack.Api/Interfaces/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using ShopFullStack.Api.Models;
+
+namespace ShopFullStack.Api.Interfaces;
+
+public interface IProductRepository : IGenericRepository<Product>
+{
+    Task<IEnumerable<Product>> GetAllWithCategoryAsync();
+    Task<Product?> GetByIdWithCategoryAsync(int id);
+}
---
+++ ShopFullStack.Api/Repositories/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopFullStack.Api.Data;
+using ShopFullStack.Api.Interfaces;
+
+namespace ShopFullStack.Api.Repositories;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    protected readonly AppDbContext _context;
+    protected readonly DbSet<T> _dbSet;
+
+    public GenericRepository(AppDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+    public async Task<T?> GetByIdAsync(int id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+    }
+
+    public void Update(T entity)
+    {
+        _dbSet.Update(entity);
+    }
+
+    public void Delete(T entity)
+    {
+        _dbSet.Remove(entity);
+    }
+
+    public async Task<bool> SaveChangesAsync()
+    {
+        return await _context.SaveChangesAsync() > 0;
+    }
+}
